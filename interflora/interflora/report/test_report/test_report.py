96609b870f46aa7aa95699e7aca98db0b56bb99c
its-alikhokher <iamaliraza777@gmail.com>
2024-03-27 01:34:11 +0500

    x

--- interflora/interflora/report/test_report/test_report.py
+++ interflora/interflora/report/test_report/test_report.py
@@ -14,13 +14,13 @@ def get_columns(filters):
             "options" : "Sales Order",
            "width":250
         },
-         {
-            "fieldname":"docname",
-            "label":("Sales"),
-            "fieldtype": "Link",
-            "options" : "Order Request",
-           "width":250
-        },
+        #  {
+        #     "fieldname":"docname",
+        #     "label":("Sales"),
+        #     "fieldtype": "Link",
+        #     "options" : "Order Request",
+        #    "width":250
+        # },
       
 	]
     return columns
@@ -43,26 +43,4 @@ def get_data(filters):
             "docname":row.name,   
         })
         # frappe.msgprint(f"{data}")
-
-
-    return data   
-
-def get_data(filters):
-    data1 = []
-    my_filter = {}
-    if filters.get("from_date") and filters.get("to_date"):
-        my_filter['creation']=['between', [filters.get("from_date"),filters.get("to_date")]]
-    if filters.get("urgent"):
-        my_filter['urgent_delivery'] = filters.get("urgent") 
-    if filters.get("type"):
-        my_filter['gift_wrapping'] = filters.get("type") 
-    if filters.get("btype"):
-        my_filter['bouquet_type'] = filters.get("btype") 
-    # frappe.msgprint(f"{my_filter}")
-    docs = frappe.get_all("Order Request",my_filter)
-    for row in docs:
-        data1.append({
-            "docname":row.name,   
-        })
-        # frappe.msgprint(f"{data}")
-    return data1  
+    return data 
