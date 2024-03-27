9b7375e4c9c53ee977f5da405fb06c9c7a7dc9a8
its-alikhokher <iamaliraza777@gmail.com>
2024-03-28 03:52:42 +0500

    x

--- interflora/event/orderreq.py
+++ interflora/event/orderreq.py
@@ -5,8 +5,9 @@ from frappe import _
 @frappe.whitelist()
 def myfunc(formData):
     data = json.loads(formData)
-    doc = frappe.new_doc("Order Request")
     # frappe.msgprint(f"{data['name']}")
+    doc = frappe.new_doc("Order Request")
+    frappe.msgprint(f"{data['bouquet_price']}")
     
 
     doc.name1 =  data['name']
@@ -16,6 +17,7 @@ def myfunc(formData):
     doc.bouquet_type = data['bouquet_type']
     doc.gift_wrapping = data['gift_wrapping']
     doc.urgent_delivery = data['urgent_delivery']
+    doc.price = data['bouquet_price']
     doc.insert()
-    doc.save()
+    # doc.save()
     
